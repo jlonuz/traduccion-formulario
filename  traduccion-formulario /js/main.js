1cbba3f6226c81f767047cbfba7f32d9e03ada70
Josefina <lonuz.j@gmail.com>
2016-10-25 11:57:09 -0300

    ejercicio terminado

---
+++  traduccion-formulario /js/main.js
@@ -0,0 +1,11 @@
+function traduccion(){
+
+	document.getElementById('form-signin-heading').innerHTML="Por favor inicia sesión";
+	document.getElementById('inputEmail').placeholder="Correo Electrónico";
+	document.getElementById('inputPassword').placeholder="Contraseña";
+	document.getElementsByTagName('span')[0].innerHTML="Recordar Datos";
+	document.getElementsByClassName('btn btn-lg btn-primary btn-block submit-btn')[0].innerHTML="Iniciar Sesión";
+
+
+
+}
